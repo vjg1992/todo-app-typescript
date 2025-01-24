5d6e13032486b9f903c67a0d9e856dab1518a62e
vjg1992 <vijayshankar871992@gmail.com>
2025-01-24 21:37:40 +0530

    Implementing changes

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+    root: true,
+    env: {
+      browser: true,
+      es2021: true,
+      node: true
+    },
+    extends: [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:react/recommended',
+      'plugin:react-hooks/recommended'
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+      ecmaFeatures: {
+        jsx: true
+      },
+      ecmaVersion: 12,
+      sourceType: 'module'
+    },
+    plugins: [
+      'react',
+      '@typescript-eslint',
+      'react-hooks'
+    ],
+    settings: {
+      react: {
+        version: 'detect'
+      }
+    },
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn'
+    }
+  }
